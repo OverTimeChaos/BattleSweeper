a99dd350c33ecee30354b070ba0b3877bb99bfa4
OverTimeChaos <jamesfan2006@hotmail.com>
2024-06-25 22:32:24 +1000

    Update Tile.gd

--- Godot project/Codes and Screens/Tile.gd
+++ Godot project/Codes and Screens/Tile.gd
@@ -8,12 +8,14 @@ const FIVE = preload ("res://Decals/MineSweeper/cells/WinmineXP/cell5.svg")
 const SIX = preload ("res://Decals/MineSweeper/cells/WinmineXP/cell6.svg")
 const SEVEN = preload ("res://Decals/MineSweeper/cells/WinmineXP/cell7.svg")
 const EIGHT = preload ("res://Decals/MineSweeper/cells/WinmineXP/cell8.svg")
-const EIGHT = preload ("res://Decals/MineSweeper/cells/WinmineXP/cell8.svg")
+const EXP = preload ("res://Decals/Explosion.svg")
 #varible for individual tile
 var Covered = true
 var Flagged = false
 var IsMine = false
 
+func _ready():
+	$Numbers.texture = EXP
 func SetMine(): #function for setting a bomb on an individual tile
 	IsMine = true #the tile has a bomb 
 	$Mine.show() #Shows "Mine" Sprite
@@ -27,23 +29,23 @@ func uncover(): #function for uncovering a tile
 			pass
 		
 		
-		elif IsMine == false: #if the tile is not a bomb, game proceeds
-			StandAlone.TilesUncovered += 1 #adds 1 to tiles opened for completion condition
-			Covered = false #Removes the "Covered" sprite
-			
-			var SurroundMineCount = 0 #displays the number of bombs around the uncovered tile in a 1 tile radius
-			for tile in get_surrounds():
-				if tile.is_bomb:
-					SurroundMineCount += 1
-			if SurroundMineCount > 0:
-				$Label.text = str(SurroundMineCount)
-			else:
-				for tile in get_surrounds():
-					if tile.is_cover:
-						tile.uncover()
-		
-		#Win condition. If bombs needed to be flagged have been flagged and tiles to be opened have been opened, player wins
-		if StandAlone.bombs_flagged == Global.bombs_number and Global.tiles_opened == Global.tiles_to_open:
-			StandAlone.first_move = false #resets first_move to default value to stop the timer
-			StandAlone.win = true
-			on_player_winlose() 
+		#elif IsMine == false: #if the tile is not a bomb, game proceeds
+			#StandAlone.TilesUncovered += 1 #adds 1 to tiles opened for completion condition
+			#Covered = false #Removes the "Covered" sprite
+			#
+			#var SurroundMineCount = 0 #displays the number of bombs around the uncovered tile in a 1 tile radius
+			#for tile in get_surrounds():
+				#if tile.is_bomb:
+					#SurroundMineCount += 1
+			#if SurroundMineCount > 0:
+				#$Label.text = str(SurroundMineCount)
+			#else:
+				#for tile in get_surrounds():
+					#if tile.is_cover:
+						#tile.uncover()
+		#
+		##Win condition. If bombs needed to be flagged have been flagged and tiles to be opened have been opened, player wins
+		#if StandAlone.bombs_flagged == Global.bombs_number and Global.tiles_opened == Global.tiles_to_open:
+			#StandAlone.first_move = false #resets first_move to default value to stop the timer
+			#StandAlone.win = true
+			#on_player_winlose() 
